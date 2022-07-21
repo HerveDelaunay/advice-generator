ff73b9998c771c46bd0721ba42054f47eace5b8d
HerveDelaunay <delaunay.herve06@gmail.com>
2022-07-21 18:45:27 +0200

    basic layout done

--- src/components/CardAdvice/CardAdvice.tsx
+++ src/components/CardAdvice/CardAdvice.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import './CardAdvice.scss'
-import { ReactComponent as PatternBreaker } from '../../assets/pattern-divider-desktop.svg'
+import patternDivider from '../../assets/pattern-divider-desktop.svg'
+import dice from '../../assets/icon-dice.svg'
 
 const CardAdvice: React.FC = () => {
   return (
-    <div className="container">
       <div className='card'>
-        <div className="card-title">Advice #117</div>
+        <div className="card-title">ADVICE #117</div>
         <div className='card-advice'>"It is easy to sit up and take notice, What is difficult is getting up and taking action."</div>
-        <PatternBreaker/>
+        <img src={patternDivider} className='card-patternDivider'/>
+        <figure className="card-circle">
+          <img src={dice} className='card-dice'/>
+        </figure>
       </div>
-    </div>
   )
 }
 
