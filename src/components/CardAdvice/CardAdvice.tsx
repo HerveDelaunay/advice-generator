4167653eb5033f63f398b3e7cc39ae9d9ab0b343
HerveDelaunay <delaunay.herve06@gmail.com>
2022-07-22 23:40:29 +0200

    vars/mixins setup + responsive wip

--- src/components/CardAdvice/CardAdvice.tsx
+++ src/components/CardAdvice/CardAdvice.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './CardAdvice.scss'
 import patternDivider from '../../assets/pattern-divider-desktop.svg'
+import patternDividerSmall from '../../assets/pattern-divider-mobile.svg'
 import dice from '../../assets/icon-dice.svg'
 
 const CardAdvice: React.FC = () => {
   return (
       <div className='card'>
-        <div className="card-title">ADVICE #117</div>
-        <div className='card-advice'>"It is easy to sit up and take notice, What is difficult is getting up and taking action."</div>
-        <img src={patternDivider} className='card-patternDivider'/>
-        <figure className="card-circle">
-          <img src={dice} className='card-dice'/>
+        <div className="card__title">ADVICE #117</div>
+        <div className='card__advice'>"It is easy to sit up and take notice, What is difficult is getting up and taking action."</div>
+        <img src={patternDivider} srcSet={`${patternDivider} 1440w, ${patternDividerSmall} 375w`}/>
+        <figure className="card__circle">
+          <img src={dice} className='card__dice'/>
         </figure>
       </div>
   )
