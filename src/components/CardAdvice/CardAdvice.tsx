dccfd14cd7bbc4d96961d0a9c483a46c1a8334b0
HerveDelaunay <delaunay.herve06@gmail.com>
2022-07-26 16:01:37 +0200

    fetch advice & active state (hover) done

--- src/components/CardAdvice/CardAdvice.tsx
+++ src/components/CardAdvice/CardAdvice.tsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CardAdvice.scss'
 import patternDivider from '../../assets/pattern-divider-desktop.svg'
 import patternDividerSmall from '../../assets/pattern-divider-mobile.svg'
 import dice from '../../assets/icon-dice.svg'
+import axios from 'axios'
 
 const CardAdvice: React.FC = () => {
+  const [advice, setAdvice] = useState<string>("")
+  const [adviceNumber, setAdviceNumber] = useState<number>()
+  const handleClick = async () => {
+    try {
+      const getAdvice = await axios({
+        method: 'get',
+        url: 'https://api.adviceslip.com/advice'
+      })
+      setAdvice(getAdvice.data.slip.advice)
+      setAdviceNumber(getAdvice.data.slip.id)
+    }
+    catch(error) {
+      console.error(error)
+    }
+  }
   return (
-      <div className='card'>
-        <div className='card__title'>ADVICE #117</div>
-        <div className='card__advice'>"It is easy to sit up and take notice, what's difficult is getting up and taking action."</div>
-        {/* {<img src={patternDivider} srcSet={`${patternDivider} 1440w, ${patternDividerSmall} 375w`}/>} */}
-        <img src={patternDivider} className='card__patternDivider'/>
-        <img src={patternDividerSmall} className='card__patternDividerSmall'/>
-        <figure className='card__circle'>
+    <div className='card'>
+      <div className='card__title'>ADVICE #{adviceNumber}</div>
+      <div className='card__advice'>"{advice}"</div>
+      <img src={patternDivider} className='card__patternDivider'/>
+      <img src={patternDividerSmall} className='card__patternDividerSmall'/>
+      <button className='card__button' onClick={handleClick}>  
           <img src={dice} className='card__dice'/>
-        </figure>
-      </div>
+      </button>
+    </div>
   )
 }
 
