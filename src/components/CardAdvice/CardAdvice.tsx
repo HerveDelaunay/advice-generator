ddcc61c810b9a0e644c98bd1a3f5a022b61565cc
HerveDelaunay <delaunay.herve06@gmail.com>
2022-07-26 13:07:31 +0200

    responsive done

--- src/components/CardAdvice/CardAdvice.tsx
+++ src/components/CardAdvice/CardAdvice.tsx
@@ -7,10 +7,12 @@ import dice from '../../assets/icon-dice.svg'
 const CardAdvice: React.FC = () => {
   return (
       <div className='card'>
-        <div className="card__title">ADVICE #117</div>
-        <div className='card__advice'>"It is easy to sit up and take notice, What is difficult is getting up and taking action."</div>
-        <img src={patternDivider} srcSet={`${patternDivider} 1440w, ${patternDividerSmall} 375w`}/>
-        <figure className="card__circle">
+        <div className='card__title'>ADVICE #117</div>
+        <div className='card__advice'>"It is easy to sit up and take notice, what's difficult is getting up and taking action."</div>
+        {/* {<img src={patternDivider} srcSet={`${patternDivider} 1440w, ${patternDividerSmall} 375w`}/>} */}
+        <img src={patternDivider} className='card__patternDivider'/>
+        <img src={patternDividerSmall} className='card__patternDividerSmall'/>
+        <figure className='card__circle'>
           <img src={dice} className='card__dice'/>
         </figure>
       </div>
